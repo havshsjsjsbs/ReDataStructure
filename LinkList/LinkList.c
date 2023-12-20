0f111207194f4bf3b03b7408e05dd42b7aebb43b
Shi Shasha <shishashalisa@163.com>
2023-12-20 10:15:02 +0800

    [修改]函数的形参和定义

---
+++ LinkList/LinkList.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "LinkList.h"
+
+/* 链表初始化 */
+int LinkListInit(LinkList ** pList);
+
+/* 链表头插 */
+int LinkListHeadInsert(LinkList * pList, ELEMENTTYPE val);
+
+/* 链表尾插 */
+int LinkListTailInsert(LinkList * pList, ELEMENTTYPE val);
+
+/* 链表指定位置插入 */
+int LinkListAppointPosInsert(LinkList * pList, int pos, ELEMENTTYPE val);
+
+/* 链表头删 */
+int LinkListHeadDel(LinkList * pList);
+
+/* 链表尾删 */
+int LinkListTailDel(LinkList * pList);
+
+/* 链表指定位置删 */
+int LinkListDelAppointPos(LinkList * pList, int pos);
+
+/* 链表指定数据删 */
+int LinkListDelAppointData(LinkList * pList, ELEMENTTYPE val);
+
+/* 获取链表的长度 */
+int LinkListGetLength(LinkList * pList, int *pSize);
+
+/* 链表的销毁 */
+int LinkListDestory(LinkList * pList);
