6670f880afd46fadd7568b7f9be5ea1ba4d131ad
Shi Shasha <shishashalisa@163.com>
2023-12-20 10:30:26 +0800

    [新增]链表初始化的API

--- LinkList/LinkList.c
+++ LinkList/LinkList.c
@@ -1,35 +1,104 @@
 #include <stdio.h>
 #include "LinkList.h"
+#include <stdlib.h>
+#include <string.h>
+
+enum STATUS_CODE
+{
+    ON_SUCCESS,
+    NULL_PTR,
+    MALLOC_ERROR,
+    INVALID_ACCESS,
+};
 
 /* 链表初始化 */
-int LinkListInit(LinkList ** pList);
+int LinkListInit(LinkList ** pList)
+{
+    int ret = 0;
+    LinkList *list = (LinkList *)(sizeof(LinkList) * 1);
+    if(list == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+    /* 清空脏数据 */
+    memset(list, 0, sizeof(LinkList) * 1);
+
+    list->head = malloc(sizeof(LinkNode) * 1);
+    if (list->head == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+    /* 清空脏数据 */
+    memset(list->head, 0, sizeof(LinkNode) * 1);
+    list->head->data = 0;
+    list->head->next = NULL;
+
+    /* 链表的长度为0 */
+    list->len = 0;
+
+    /* 二级指针 */
+    *pList = list;
+    return ret;
+
+
+}
 
 /* 链表头插 */
-int LinkListHeadInsert(LinkList * pList, ELEMENTTYPE val);
+int LinkListHeadInsert(LinkList * pList, ELEMENTTYPE val)
+{
+
+}
 
 /* 链表尾插 */
-int LinkListTailInsert(LinkList * pList, ELEMENTTYPE val);
+int LinkListTailInsert(LinkList * pList, ELEMENTTYPE val)
+{
+
+}
 
 /* 链表指定位置插入 */
-int LinkListAppointPosInsert(LinkList * pList, int pos, ELEMENTTYPE val);
+int LinkListAppointPosInsert(LinkList * pList, int pos, ELEMENTTYPE val)
+{
+
+}
 
 /* 链表头删 */
-int LinkListHeadDel(LinkList * pList);
+int LinkListHeadDel(LinkList * pList)
+{
+
+}
 
 /* 链表尾删 */
-int LinkListTailDel(LinkList * pList);
+int LinkListTailDel(LinkList * pList)
+{
+
+}
 
 /* 链表指定位置删 */
-int LinkListDelAppointPos(LinkList * pList, int pos);
+int LinkListDelAppointPos(LinkList * pList, int pos)
+{
+
+}
 
 /* 链表指定数据删 */
-int LinkListDelAppointData(LinkList * pList, ELEMENTTYPE val);
+int LinkListDelAppointData(LinkList * pList, ELEMENTTYPE val)
+{
+
+}
 
 /* 获取链表的长度 */
-int LinkListGetLength(LinkList * pList, int *pSize);
+int LinkListGetLength(LinkList * pList, int *pSize)
+{
+
+}
 
 /* 链表的销毁 */
-int LinkListDestory(LinkList * pList);
+int LinkListDestory(LinkList * pList)
+{
+
+}
 
 /* 链表遍历接口 */
-int LinkListDestory(LinkList * pList);
+int LinkListDestory(LinkList * pList)
+{
+
+}
