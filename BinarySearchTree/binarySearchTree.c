5d4dd61732db06364ba132499e4c8cb539cde664
Shi Shasha <shishashalisa@163.com>
2023-12-25 15:13:40 +0800

    [新增] 新增树的结点和初始化和插入的API接口

---
+++ BinarySearchTree/binarySearchTree.c
@@ -0,0 +1,13 @@
+#include "binarySearchTree.h"
+
+/* 二叉搜索树的初始化 */
+int binarySearchTreeInit(BinarySearchTree **pBstree)
+{
+
+}
+
+/* 二叉搜索树的插入 */
+int binarySearchTreeInsert(BinarySearchTree **pBstree, ELEMENTTYPE val)
+{
+
+}
